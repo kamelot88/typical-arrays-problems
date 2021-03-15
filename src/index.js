b5bf5a73c5f7c2d1b4391b19a722d84f986b4cad
kamelot88 <vasilchenkoOV1988@gmail.com>
2021-03-15 23:37:21 +0200

    finish

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
 exports.min = function min (array) {
-  return 0;
+  let sortAr = array.sort((a, b) => a-b);
+  return sortAr[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  let sortAr = array.sort((a, b) => a-b).reverse();
+  return sortAr[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let sortAr = array.reduce((acc, item) => acc + item) / array.length;
+  return sortAr;
 }
+
+
+
+  
